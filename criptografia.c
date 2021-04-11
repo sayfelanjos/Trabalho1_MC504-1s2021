206f19cfcc21c0f645f9d8a182f35bb0bbc6ee6c
SaymonFelipe <saymonfelipe@hotmail.com>
2021-04-11 20:35:38 -0300

    cria funcão de multiplicação

--- criptografia.c
+++ criptografia.c
@@ -1,3 +1,8 @@
-/**
- * Implementar as funções aqui
- */
+#include "criptografia.h"
+
+void* multiplicacao(void* parametros) {
+    Parameters_multiplicacao *p = (Parameters_multiplicacao *) parametros;
+    for (int i = 0; i < p->num_linhas; i++)
+        for (int j = 0; j < 4; j++)
+            p->matriz_codigo[i][p->coluna] += p->matriz_texto[i][j] * p->criptografia[j][p->coluna];
+}
--- criptografia.h
+++ criptografia.h
@@ -1,20 +1,21 @@
-struct parameters_multiplicacao {
-	int linha_inicial; // numero da primeira linha da matriz resultante da entrada que sera multiplicada pela thread
-	int linha_final; // numero da ultima linha da matriz resultante da entrada que sera multiplicada pela thread
-	int coluna_inicial; // numero da coluna da primeira coluna da matriz de criptografia que sera multiplicada pela thread
-	int coluna_final; //numero da ultima coluna da matriz de criptografia que sera multiplicada pela thread
-	int *** matriz_codigo; // endereco da matriz que sera usada para armazenar o resultado final da multiplicacao
-};
+#include <stdio.h>
+#include <stdlib.h>
 
-structure parameters_transforma_texto {
+typedef struct {
+	int criptografia[4][4];
+	int **matriz_texto;
+	int num_linhas; // numero de linhas da matriz a ser criptografada.
+	int coluna; // numero da coluna que está sendo criptografada
+	int **matriz_codigo; // endereco da matriz que sera usada para armazenar o resultado final da multiplicacao
+} Parameters_multiplicacao;
 
+struct parameters_transforma_texto {
 	int*** matriz_resultado; // endereco da matriz que armazena o resultado
 	int caracter_inicial; // indice do primeiro caracter que sera mapeado pela thread
 	int caracter_final; // indice do ultimo caracter que sera mapeado pela thread
 };
 
-structure parameters_transforma_codigo {
-
+struct parameters_transforma_codigo {
 	char** texto; // endereco da string que vai armazenar o resultado
 	int coluna_inicial; // indice da primeira coluna que sera mapeada pela thread;
 	int coluna_final; // indice da ultima coluna que sera mapeada pela thread;
@@ -22,14 +23,21 @@ structure parameters_transforma_codigo {
 	int linha_final; // indice da ultima linha que sera mapeada pela thread;
 };
 
-typedef structure parameters_multiplicacao parameters_multiplicacao; //parametros para a funcao multiplicacao
+/**
+ * funcao usada pela thread pra multiplicar a 
+ * matriz de entrada pela matriz de criptografia
+ */
+void* multiplicacao(void* parametros); 
 
-typedef structure parameters_transforma_texto parameters_transforma_texto; //parametros para a funcao transforma texto
+/** 
+ * funcao que transforma a mensagem de entrada em 
+ * uma matriz numerica; realiza o maping entre as 
+ * letras da mensagem e numeros
+ */
+void* transforma_texto(void* texto); 
 
-typedef structure parameters_transforma_codigo parameters_transforma_codigo; //parametros para a funcao transforma codigo
-
-void* multiplicacao(void* parametros); //funcao usada pela thread pra multiplicar a matriz de entrada pela matriz de criptografia
-
-void* transforma_texto(void* texto); // funcao que transforma a mensagem de entrada em uma matriz numerica; realiza o maping entre as letras da mensagem e numeros
-
-void* transforma_codigo(void* codigo); // funcao que transforma o codigo criptografado de volta em texto; realiza o maping entre numeros do codigo e letras
+/**
+ * funcao que transforma o codigo criptografado de volta em texto; 
+ * realiza o maping entre numeros do codigo e letras
+ */
+void* transforma_codigo(void* codigo); 
